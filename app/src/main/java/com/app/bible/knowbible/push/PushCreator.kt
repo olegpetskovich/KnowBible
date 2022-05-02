f8bf7b388de6cffe10afd0d290710fe239afbe68
olegpetskovich <olegpetskovich@gmail.com>
2022-05-02 10:42:55 +0300

    Edited push sending

--- app/src/main/java/com/app/bible/knowbible/push/PushCreator.kt
+++ app/src/main/java/com/app/bible/knowbible/push/PushCreator.kt
@@ -10,7 +10,6 @@ import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 import com.app.bible.knowbible.R
-import com.app.bible.knowbible.push.accessory.cancelGoal
 import com.app.bible.knowbible.push.accessory.clickGoal
 import com.app.bible.knowbible.push.accessory.isRestrictedByTimeAndState
 
@@ -24,11 +23,7 @@ object PushCreator {
         val nm = context.getSystemService(Service.NOTIFICATION_SERVICE) as NotificationManager
         nm.createPushChannel()
         val body = RemoteViews(context.packageName, R.layout.push_layout)
-        body.apply {
-            setOnClickPendingIntent(R.id.btnRead, context.clickGoal())
-            setOnClickPendingIntent(R.id.rootR, context.clickGoal())
-            setOnClickPendingIntent(R.id.btnCancel, context.cancelGoal())
-        }
+        body.apply { setOnClickPendingIntent(R.id.btnRead, context.clickGoal()) }
 
         val ncb = context.returnBuilder(body)
         nm.notify(pushId, ncb.build())
@@ -51,6 +46,15 @@ object PushCreator {
     private fun Context.returnBuilder(
         body: RemoteViews,
     ): NotificationCompat.Builder {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            return NotificationCompat.Builder(this, notifChannelName)
+                .setSmallIcon(android.R.color.transparent)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setColor(ContextCompat.getColor(applicationContext, android.R.color.white))
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setAutoCancel(true)
+                .setCustomContentView(body)
+        }
         return when (Build.MANUFACTURER.toString()) {
             "HUAWEI" -> NotificationCompat.Builder(this, notifChannelName)
                 .setSmallIcon(android.R.color.transparent)
@@ -58,8 +62,7 @@ object PushCreator {
                 .setColor(ContextCompat.getColor(applicationContext, android.R.color.white))
                 .setAutoCancel(true)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setCustomBigContentView(body)
-                .setCustomHeadsUpContentView(body)
+                .setCustomContentView(body)
 
             else -> NotificationCompat.Builder(this, notifChannelName)
                 .setSmallIcon(android.R.color.transparent)
@@ -68,8 +71,7 @@ object PushCreator {
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setAutoCancel(true)
                 .setFullScreenIntent(clickGoal(), true)
-                .setCustomBigContentView(body)
-                .setCustomHeadsUpContentView(body)
+                .setCustomContentView(body)
         }
     }
 }
