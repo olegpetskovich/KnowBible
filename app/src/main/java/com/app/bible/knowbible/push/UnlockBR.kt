cd1942b22a56457e8d3c991d77fd017c139537db
olegpetskovich <olegpetskovich@gmail.com>
2022-05-04 11:27:09 +0300

    Prepared notifications sending

--- app/src/main/java/com/app/bible/knowbible/push/UnlockBR.kt
+++ app/src/main/java/com/app/bible/knowbible/push/UnlockBR.kt
@@ -5,13 +5,11 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import android.os.Build
-import android.widget.RemoteViews
-import com.app.bible.knowbible.R
 import com.app.bible.knowbible.utility.SaveLoadData
 import com.app.bible.knowbible.utility.Utility.Companion.getCurrentTime
-import com.app.bible.knowbible.utility.Utility.Companion.getTapTargetButton
-
+import com.app.bible.knowbible.utility.Utility.Companion.log
+import java.text.SimpleDateFormat
+import java.util.*
 
 class UnlockBR : BroadcastReceiver() {
     companion object {
@@ -26,15 +24,15 @@ class UnlockBR : BroadcastReceiver() {
         if (intent.action == Intent.ACTION_USER_PRESENT) {
             val kgMan = context.getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
             if (kgMan.isKeyguardLocked.not()) {
-                    //                val saveLoadData = SaveLoadData(context)
-//
-//                val timeInHours = SimpleDateFormat("H", Locale.getDefault()).format(Date()).toInt()
-//                log(timeInHours.toString())
-//
-//                if (isNewDay(saveLoadData) && timeInHours >= startPushTime && timeInHours <= endPushTime) {
+                val saveLoadData = SaveLoadData(context)
+
+                val timeInHours = SimpleDateFormat("H", Locale.getDefault()).format(Date()).toInt()
+                log(timeInHours.toString())
+
+                if (isNewDay(saveLoadData) && timeInHours >= startPushTime && timeInHours <= endPushTime) {
                     PushCreator.send(context)
-//                    saveLoadData.saveLong(SHOWED_PUSH_TIME_KEY, getCurrentTime())
-//                }
+                    saveLoadData.saveLong(SHOWED_PUSH_TIME_KEY, getCurrentTime())
+                }
             }
         }
     }
@@ -46,6 +44,10 @@ class UnlockBR : BroadcastReceiver() {
         context.registerReceiver(this, filter)
     }
 
-    private fun isNewDay(saveLoadData: SaveLoadData) =
-        (getCurrentTime() - saveLoadData.loadLong(SHOWED_PUSH_TIME_KEY)) >= dayLength
+    private fun isNewDay(saveLoadData: SaveLoadData): Boolean {
+        val curTime = getCurrentTime()
+        val savedTime = saveLoadData.loadLong(SHOWED_PUSH_TIME_KEY)
+        val result = curTime - savedTime
+        return result >= dayLength
+    }
 }
